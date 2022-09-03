b2dd34c01e81e6f7c0aff5558cf1b729324bc063
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-02 21:58:54 -0300

    :sparkles: feat(Program.cs): create interactive menu, simulating a product management

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+int option;
+while (true)
+{
+    Console.WriteLine($"\n1 - Add a product");
+    Console.WriteLine($"2 - Update a product");
+    Console.WriteLine($"3 - Search a product");
+    Console.WriteLine($"4 - Delte a product");
+    Console.WriteLine($"5 - Terminate session");
+    Console.WriteLine($"Choose a option:");
+    option = Convert.ToInt16(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine($"Product added");
+            break;
+        case 2:
+            Console.WriteLine($"Product updated");
+            break;
+        case 3:
+            Console.WriteLine($"Product found");
+            break;
+        case 4:
+            Console.WriteLine($"Product deleted");
+            break;
+        case 5:
+            Console.WriteLine($"end session");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine($"invalid option");
+            break;
+    }
+}
